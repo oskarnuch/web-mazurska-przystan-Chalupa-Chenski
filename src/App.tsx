0af4abe8f5d4bcd45d182dcca969941a772cb125
Mania <maria.chalupa@cyfroweszkoly.pl>
2026-03-13 11:01:27 +0100

    created form with labels, inputs, checkoboxes and selects

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <form>
+            <label>Podaj swoje imię: <input type="text" /*value={imie}*//></label><br/>
+            <label>Wybierz, co chcesz wypożyczyć:<br/>
+                <input type="radio" value="kajak"/>Kajak (20zł/h)<br/>
+                <input type="radio" value="rower"/>Rower wodny (35zł/h)<br/>
+                <input type="radio" value="omega"/>Omega (150zł/h)  !WYMAGAMY PATENTU!<br/>
+            </label>
+            <label>Na ile godzin chcesz wynająć sprzęt?<br/>
+                <div style={{display: "flex", alignItems: "center", gap: "10px"}}>
+                <input type="range"
+                       min={1}
+                       max={8}
+                       step={1}
+                       /*value={hours}*/
+                /><span style={{fontFamily: "Arial", fontStyle:"oblique"}}>godzin...</span>
+                </div></label><br/>
+            <label><input type="checkbox"/>Chcę kapok dla dziecka (5zł)</label><br/>
+            <label><input type="checkbox"/>Chcę pływać z instruktorem (50zł/h)</label><br/>
+            <label>Płacę <select>
+                <option value="card">kartą</option>
+                <option value="BLIK">BLIK-iem</option>
+            </select></label><br/>
+            <label><input type="checkbox"/>Akceptuję regulamin przystani</label>
+        </form>
     </>
   )
 }
