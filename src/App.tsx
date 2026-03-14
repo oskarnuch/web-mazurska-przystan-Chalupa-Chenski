4194dc2da108338196d34804406c2e5b0a2adffc
Oskarnuch <oskar.chenski@cyfroweszkoly.pl>
2026-03-14 19:01:58 +0100

    Add event handlers and basic UI for test + button validaton

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import {type ChangeEvent, useState} from 'react'
 import './App.css'
 
 type Sprzet = 'kajak' | 'rower' | 'omega'
 
 function App() {
 
+    //usestate
     const [imie, setImie] = useState<string>('');
     const [sprzet, setSprzet] = useState<Sprzet>('kajak');
     const [godziny, setGodziny] = useState<number>(1);
@@ -13,6 +14,23 @@ function App() {
     const [platnosc, setPlatnosc] = useState<'Karta' | 'blik'>('blik');
     const [regulamin, setRegulamin] = useState<boolean>(true);
 
+    //handlery
+    const handleName =
+        (e: ChangeEvent<HTMLInputElement>) => setImie(e.target.value);
+    const handleSprzet =
+        (e: ChangeEvent<HTMLSelectElement>) => setSprzet(e.target.value as Sprzet);
+    const handleHours =
+        (e: ChangeEvent<HTMLInputElement>) => setGodziny(Number(e.target.value));
+    const handleKapok =
+        (e: ChangeEvent<HTMLInputElement>) => setKapok(e.target.checked);
+    const handleInstruktor =
+        (e: ChangeEvent<HTMLInputElement>) => setInstruktor(e.target.checked);
+    const handlePlatnosc =
+        (e: ChangeEvent<HTMLSelectElement>) => setPlatnosc(e.target.value as 'Karta' | 'blik');
+    const handleRegulamin =
+        (e: ChangeEvent<HTMLInputElement>) => setRegulamin(e.target.checked);
+
+    //funkcja
     const calculateTotal = (): number => {
         const ceny = {
             kajak: 20,
@@ -23,9 +41,25 @@ function App() {
         if (instruktor) suma += 50 * godziny;
         if (kapok) suma += 5;
         return suma;
-    }
+    };
 
+    const isFormValid = imie.length > 0 && regulamin;
 
+    //html
+    return (
+        <>
+            <input value={imie} onChange={handleName} />
+            <select value={sprzet} onChange={handleSprzet}>...</select>
+            <input type="range" value={godziny} onChange={handleHours} />
+            <input type="checkbox" checked={kapok} onChange={handleKapok} />
+            <input type="checkbox" checked={instruktor} onChange={handleInstruktor} />
+            <select value={platnosc} onChange={handlePlatnosc}>...</select>
+            <input type="checkbox" checked={regulamin} onChange={handleRegulamin} />
+
+            Wynik: {calculateTotal()} zł
+            Przycisk: <button disabled={!isFormValid}>Rezerwuję</button>
+        </>
+    );
 }
 
 export default App
