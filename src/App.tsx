3077405e80278019c1600aafcc4033d5ee1e02c2
Oskarnuch <oskar.chenski@cyfroweszkoly.pl>
2026-03-19 18:54:28 +0100

    Add final css and html code from Marysia

--- src/App.tsx
+++ src/App.tsx
@@ -56,37 +56,53 @@ function App() {
     return (
         <>
             <div>
-                <h1>Przystań Mazurska</h1>
-                <form style={{display: "flex", flexDirection: "column", justifyContent:"center"}}
-                onSubmit={handleSubmit}>
+                <span id="offer" style={{flex: 3}}>
+            <h2>Nasza oferta:</h2>
+                <h3>Sprzęt pływający</h3>
+            <ul>
+                <li>Kajaki - 20zł/h</li>
+                <li>Rowerki wodne - 35zł/h</li>
+                <li>Omega 150zł/h <br/><b>!WYMAGANY PATENT!</b></li>
+            </ul>
+                <h3>Dodatki</h3>
+                <ul>
+                    <li>Kapok dziecięcy - 5zł</li>
+                    <li>Instruktor - 50zł/h</li>
+                </ul>
+        </span>
+                <span id="forms" style={{flex: 5}}>
+                <h2>Wypełnij formularz rezerwacyjny:</h2>
+            <form style={{display: "flex", flexDirection: "column", justifyContent:"center"}}
+                  onSubmit={handleSubmit}>
 
-                    <label>Podaj swoje imię: <input type="text" value={imie} onChange={handleName}/></label><br/>
-                    <label>Wybierz, co chcesz wypożyczyć:<br/>
-                        <select value={sprzet} onChange={handleSprzet}>
-                            <option value="kajak">Kajak (20zł/h)</option>
-                            <option value="rower">Rower wodny (35zł/h)</option>
-                            <option value="omega">Omega (150zł/h)  !WYMAGAMY PATENTU!</option>
-                        </select>
-                    </label><br/>
-                    <label>Na ile godzin chcesz wynająć sprzęt?<br/>
-                        <input type="range"
-                               min={1}
-                               max={8}
-                               step={1}
-                            value={godziny} onChange={handleHours}
-                        /> godzin...</label><br/>
-                    <label><input type="checkbox" checked={kapok} onChange={handleKapok}/>Chcę kapok dla dziecka (5zł)</label><br/>
-                    <label><input type="checkbox" checked={instruktor} onChange={handleInstruktor}/>Chcę pływać z instruktorem (50zł/h)</label><br/>
-                    <label>Płacę <select value={platnosc} onChange={handlePlatnosc}>
-                        <option value="karta">kartą</option>
-                        <option value="BLIK">BLIK-iem</option>
-                    </select></label><br/>
-                    <label><input type="checkbox" checked={regulamin} onChange={handleRegulamin}/>Akceptuję regulamin przystani</label>
-                    <button type="submit" disabled={!isFormValid}>Zarezerwuj</button>
-                </form>
-                <h2>Podsumowanie:</h2>
-                <p>Cena końcowa: <strong>{finalnaCena} zł</strong></p>
+                <label>Podaj swoje imię: <input type="text" value={imie} onChange={handleName}/></label><br/>
+                <label>Wybierz, co chcesz wypożyczyć: <select value={sprzet} onChange={handleSprzet}>
+                        <option value="kajak">Kajak</option>
+                        <option value="rower">Rower wodny</option>
+                        <option value="omega">Omega !WYMAGAMY PATENTU!</option>
+                    </select>
+                </label><br/>
+                <label>Na ile godzin chcesz wynająć sprzęt?<br/>
+                    <input type="range"
+                           min={1}
+                           max={8}
+                           step={1}
+                           value={godziny} onChange={handleHours}
+                    />{godziny} h</label><br/>
+                <label><input type="checkbox" checked={kapok} onChange={handleKapok}/>Chcę kapok dla dziecka</label><br/>
+                <label><input type="checkbox" checked={instruktor} onChange={handleInstruktor}/>Chcę pływać z instruktorem</label><br/>
+                <label>Płacę <select value={platnosc} onChange={handlePlatnosc}>
+                    <option value="karta">kartą</option>
+                    <option value="BLIK">BLIK-iem</option>
+                </select></label><br/>
+                <label><input type="checkbox" checked={regulamin} onChange={handleRegulamin}/>Akceptuję regulamin przystani</label>
+                <button type="submit" disabled={!isFormValid}>Zarezerwuj</button>
+            </form>
+            <h2>Podsumowanie:</h2>
+            <p>Cena końcowa: <strong>{finalnaCena} zł</strong></p>
+        </span>
             </div>
+
         </>
     );
 }
